28b446c9a3ebb890c1952bd4399023420e4e590a
Md Minhazul Arifin <69733252+Arifin-rafe@users.noreply.github.com>
2026-03-07 23:52:44 +0600

    interview frontend 77 done

---
+++ frontend77.js
@@ -0,0 +1,23 @@
+// console.log("minhaz"-"rafe")
+
+// const str1 = "minhaz"
+// const str2 = "rafe"
+// function stringAdd(s1, s2) {
+//     let result = "";
+//     let min = Math.min(s1.length, s2.length);
+//     for (let i = 0; i < min; i++) {
+//         result += s1[i] + s2[i];
+//     }
+//     return result;
+// }
+// console.log(stringAdd(str1, str2));
+
+// let a = 10;
+// let b = 30;
+// function swap(x, y) {return [a, b] = [y, x] }
+// console.log(swap(a, b));
+// promise vs async await?
+// A Promise is a JavaScript object that represents the eventual completion (or failure) of an asynchronous operation,
+// and its resulting value.
+// An async function is a function declared with the async keyword, which returns a Promise.
+// Inside an async function, you can use the await keyword to pause execution until a Promise is resolved.
