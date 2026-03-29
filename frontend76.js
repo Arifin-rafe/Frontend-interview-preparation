676ce1229d725f5e1bcde192537974896816f00a
Md Minhazul Arifin <69733252+Arifin-rafe@users.noreply.github.com>
2026-03-29 23:03:58 +0600

    interview 76 more

--- frontend76.js
+++ frontend76.js
@@ -1,3 +1,8 @@
-let arr = [18,10,12,5,9,11]
-console.log(arr.splice(2,4))
-console.log(arr)
+// let arr = [18,10,12,5,9,11]
+// console.log(arr.splice(2,4))
+// console.log(arr)
+
+// slice vs splice?
+// In JavaScript, slice() returns a shallow copy of a portion of an array without modifying the original.
+// In contrast, splice() changes the original array by adding, removing, or replacing elements.
+// Use slice() for copying and splice() for editing the array.
